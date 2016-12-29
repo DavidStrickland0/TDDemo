203847e8c152373244bc953f7d595546dd539b0b
davidstrickland0 <davidstrickland0@hotmail.com>
2016-12-29 00:13:44 -0500

    Rules test #1

--- XamarinTDDemo/XamarinTDDemo.Babysitter.Tests/CalculatorRulesTests.cs
+++ XamarinTDDemo/XamarinTDDemo.Babysitter.Tests/CalculatorRulesTests.cs
@@ -14,7 +14,20 @@ namespace XamarinTDDemo.Babysitter.Tests
         [TestMethod]
         public void paid_for_full_hours_no_fractional_hours()
         {
-            Assert.Fail();
+            //Setup
+            Timings timings = Data.Defaults.Timings();
+            Rates rates = Data.Defaults.Rates();
+            decimal expected =
+                timings.PreBedtime.Hours * rates.PreBedtime +
+                timings.BedtimeToMidnight.Hours * rates.BedtimeToMidnight +
+                timings.MidnightToEndOfJob.Hours * rates.MidnightToEndOfJob;
+            
+            //Modify
+            timings.BedtimeToMidnight = timings.BedtimeToMidnight.Subtract(new TimeSpan(0, 15, 0));
+
+            //Assert
+            decimal actual = Calculator.Calulate(rates, timings);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
--- XamarinTDDemo/XamarinTDDemo.Babysitter.Tests/CalculatorStoryTests.cs
+++ XamarinTDDemo/XamarinTDDemo.Babysitter.Tests/CalculatorStoryTests.cs
@@ -9,17 +9,17 @@ namespace XamarinTDDemo.Babysitter.Tests
         [TestMethod]
         public void Story_Test_calculate_my_nightly_charge()
         {
+            //Setup
             Timings timings = Data.Defaults.Timings();
             Rates rates = Data.Defaults.Rates();
-
-            decimal actual = Calculator.Calulate(rates, timings);
 
             decimal expected =
                 timings.PreBedtime.Hours * rates.PreBedtime +
                 timings.BedtimeToMidnight.Hours * rates.BedtimeToMidnight +
                 timings.MidnightToEndOfJob.Hours * rates.MidnightToEndOfJob;
 
-
+            //Assert
+            decimal actual = Calculator.Calulate(rates, timings);
             Assert.AreEqual(expected, actual);
         }
 
