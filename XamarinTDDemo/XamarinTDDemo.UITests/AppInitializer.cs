24a416f6ac694a69bd49d9ab9825eedf8f7ad0ca
David Strickland <davidstrickland0@hotmail.com>
2017-01-02 15:00:25 +0530

    Added Xamarin Droid APP. Tested for AppLaunch.

---
+++ XamarinTDDemo/XamarinTDDemo.UITests/AppInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace XamarinTDDemo.UITests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile("../../../XamarinTDDUI.UI/XamarinTDDUI.Droid/bin/Debug/XamarinTDDUI.Droid.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
